7ae955dd1b427dfce957235439ea4a90367d8c68
safarovdavlat240-tech <safarovdavlat240@gmail.com>
2026-09-22 16:17:59 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,14 @@
 import os
-import threading
-from flask import Flask
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-TOKEN = os.environ.get("BOT_TOKEN")
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Салом! Бот кор мекунад! 🚀")
-app_flask = Flask(__name__)
-@app_flask.route('/')
-def home():
-    return "Bot is alive!"
-def run_flask():
-    port = int(os.environ.get("PORT", 10000))
-    app_flask.run(host='0.0.0.0', port=port)
-def main():
-    threading.Thread(target=run_flask).start()
-    application = Application.builder().token(TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
-    application.run_polling()
+
 if __name__ == '__main__':
-    main()
+    token = os.getenv("BOT_TOKEN")
+    print(f"Token bor: {bool(token)}")
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot is alive!")
+    app.run_polling()
